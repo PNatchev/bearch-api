45a9b60b00ecd1fee8142cc06d238bee0fa9388d
Plamen Natchev <natchev.plamen@gmail.com>
2023-03-23 12:59:31 -0500

    test

--- src/main/java/io/api/bearch/BearchApplication.java
+++ src/main/java/io/api/bearch/BearchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BearchApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(BearchApplication.class, args);
     }
