68da8c6cae634cfa1369e2cfc370cc307944529b
Plamen Natchev <natchev.plamen@gmail.com>
2023-03-23 12:52:22 -0500

    Initial commit

---
+++ src/main/java/io/api/bearch/BearchApplication.java
@@ -0,0 +1,13 @@
+package io.api.bearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearchApplication.class, args);
+    }
+
+}
